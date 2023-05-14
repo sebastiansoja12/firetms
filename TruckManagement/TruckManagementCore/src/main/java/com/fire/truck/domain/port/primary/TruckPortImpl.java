b140bf79e7a049726f814d8b5749d7ed401ca152
Sebastian <sebastian5253@wp.pl>
2023-05-14 18:23:01 +0200

    Delete unused imports

--- TruckManagement/TruckManagementCore/src/main/java/com/fire/truck/domain/port/primary/TruckPortImpl.java
+++ TruckManagement/TruckManagementCore/src/main/java/com/fire/truck/domain/port/primary/TruckPortImpl.java
@@ -1,15 +1,11 @@
 package com.fire.truck.domain.port.primary;
 
 import com.fire.truck.domain.model.Truck;
-import com.fire.truck.domain.model.TruckPositionResponse;
 import com.fire.truck.domain.model.TruckRequest;
 import com.fire.truck.domain.service.TruckService;
 import lombok.AllArgsConstructor;
-import org.springframework.util.ObjectUtils;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Stream;
 
 @AllArgsConstructor
 public class TruckPortImpl implements TruckPort {
