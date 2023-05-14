e7c4665ffdc062b541777fe17bb806ddcf756f31
Sebastian <sebastian5253@wp.pl>
2023-05-14 15:33:44 +0200

    Add prod database

--- TokenStage/src/main/java/com/fire/telemetry/TelemetryProperties.java
+++ TokenStage/src/main/java/com/fire/telemetry/TelemetryProperties.java
@@ -1,11 +1,7 @@
 package com.fire.telemetry;
 
 import lombok.Getter;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Service;
 
 @Getter
 @ConfigurationProperties(prefix = "telemetry")
